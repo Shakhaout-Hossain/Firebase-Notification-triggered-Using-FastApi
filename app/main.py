ec00a76824c0cd757b508d248c0cd6de69bbd4d8
Rimon <rimonruet@gmail.com>
2025-07-24 09:48:19 +0600

    add cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.notification import router as notification_router
 
 app = FastAPI(title="Firebase FCM FastAPI")
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change "*" to a list of allowed domains in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(notification_router, prefix="/api/v1/notification", tags=["Notification"])
